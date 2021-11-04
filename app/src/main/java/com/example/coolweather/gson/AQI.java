00dbde9156fe28c3bf6f949761e688f2d8bf0b40
lzy <liangzhenyu@kuaishou.com>
2021-11-04 18:38:22 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,17 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AQI {
+
+    @SerializedName("city")
+    public AQICity city;
+
+    public class AQICity{
+        @SerializedName("aqi")
+        public String aqi;
+
+        @SerializedName("pm25")
+        public String pm25;
+    }
+}
